ff42fa1c58b33a460c06726fc9ca0a7c14b5bf76
henriquejosefaria <a82200@alunos.uminho.pt>
2020-06-11 14:38:21 +0100

    update com os restantes ficheiros necessários

---
+++ cmd_config.pl
@@ -0,0 +1,11 @@
+
+#Ficheiro de configuração do URL do WSDL a utilizar e da APPLICATION_ID fornecida pela AMA.
+
+
+# ApplicationId da entidade, fornecida pela AMA
+$APPLICATION_ID = 'Change to your ApplicationId'
+
+#Devolve APPLICATION_ID (fornecida pela AMA).
+sub get_appid{
+	return $APPLICATION_ID
+}
---
+++ test_cmd_wsdl.pl
@@ -0,0 +1,8 @@
+
+use cmd_config;
+
+$APPLICATION_ID = cmd_config.get_appid()
+
+sub main{
+
+}
